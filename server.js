c10e92eb54ba151ab4217ec5a6d176ac5394d668
lappbsstore-cell <lappbs.store@gmail.com>
2026-03-11 13:14:05 -0600

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/contingencia", async (req, res) => {
 
     const texto = r.data.toLowerCase();
 
-    let estado = "ninguna";
+    let estado = "faseI";
 
     if (texto.includes("fase ii")) estado = "faseII";
     else if (texto.includes("fase i")) estado = "faseI";
@@ -43,4 +43,5 @@ app.listen(3000, () => {
   console.log("API contingencia lista en puerto 3000");
 
 });
+
 

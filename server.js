aa8a999515de1d0795190f162b44d82b5584a09a
lappbsstore-cell <lappbs.store@gmail.com>
2026-03-11 11:53:04 -0600

    Add files via upload
    API contingencia inicial

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.get("/contingencia", async (req, res) => {
+
+  try {
+
+    const r = await axios.get(
+  "https://www.gob.mx/comisionambiental/prensa",
+  {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
+    }
+  }
+);
+
+    const texto = r.data.toLowerCase();
+
+    let estado = "ninguna";
+
+    if (texto.includes("fase ii")) estado = "faseII";
+    else if (texto.includes("fase i")) estado = "faseI";
+
+    res.json({
+      estado: estado
+    });
+
+  } catch (e) {
+
+    res.json({
+      estado: "error"
+    });
+
+  }
+
+});
+
+app.listen(3000, () => {
+  console.log("API contingencia lista en puerto 3000");
+});
